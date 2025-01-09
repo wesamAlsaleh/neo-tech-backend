a0e04b90b1a15e345394f835dd5cdedea7a6a674
wesamAlsaleh <wesammuneer7@gmail.com>
2025-01-10 00:48:56 +0300

    refactor User model and migration to use first_name and last_name; add role and phone_number fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'role',
+        'phone_number',
     ];
 
     /**
