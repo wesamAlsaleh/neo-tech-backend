4e31337fa3e9f07355c21cf868bec2a78033aeca
wesamAlsaleh <wesammuneer7@gmail.com>
2025-04-20 17:59:23 +0300

    Add hashed_id in all order array's (in all responses from the orderController)

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+// This trait is used to generate UUIDs for the model
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
 class Order extends Model
 {
+    // This trait is used to generate UUIDs for the model
+    use HasUuids;
+
+    /**
+     * The attributes that will be appended to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['hashed_id'];
+
     /**
      * The attributes that are mass assignable.
      *
@@ -33,5 +46,16 @@ class Order extends Model
     public function orderItems()
     {
         return $this->hasMany(OrderItem::class);
+    }
+
+    /**
+     * Accessor: Get the hashed ID of the order.
+     *
+     * @return string
+     */
+    public function getHashedIdAttribute()
+    {
+        // Return the hashed ID of the order
+        return hash('sha256', $this->id);
     }
 }
